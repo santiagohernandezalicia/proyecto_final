01f4663e86ba90ed293ee228c4ce4781901f9820
otman <otman.gmqz@gmail.com>
2025-05-06 10:24:23 -0700

    librerias agregadas

---
+++ diccionario_revistas.py
@@ -0,0 +1,17 @@
+import os
+import csv
+import json
+''' Lector de archivo CSV y creador de diccionario de revistas '''
+
+def leer_csv(folder_path, tipo):
+    ''' Función para leer archivo CSV'''
+
+def crear_diccionario():
+    ''' Función para crear el diccionario de revistas '''
+
+def main():
+    ''' Función principal '''
+
+if __name__ == "__main__":
+    ''' Llamar a la función principal '''
+    main()
